103d7e6771fafe4efef7309eb9df63a2d6b99e41
Evan Lindsay <evanlindsay@live.com>
2013-12-23 12:53:06 -0800

    Load as a Bukkit plugin

---
+++ src/main/java/com/gmail/favorlock/CommonUtils.java
@@ -0,0 +1,13 @@
+package com.gmail.favorlock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommonUtils extends JavaPlugin {
+
+    public static CommonUtils plugin;
+
+    public void onEnable() {
+        plugin = this;
+    }
+
+}
