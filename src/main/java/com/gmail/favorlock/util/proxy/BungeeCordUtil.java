179d9590693eb96defebd88ff7e72ef2d46c6b37
Evan Lindsay <evanlindsay@live.com>
2013-12-27 05:43:15 -0800

    BungeeCord Support: sendPlayerToServer

---
+++ src/main/java/com/gmail/favorlock/util/proxy/BungeeCordUtil.java
@@ -0,0 +1,28 @@
+package com.gmail.favorlock.util.proxy;
+
+import com.gmail.favorlock.CommonUtils;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class BungeeCordUtil {
+
+    public static void sendPlayerToServer(Player player, String server) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(baos);
+
+            dos.writeUTF("Connect");
+            dos.writeUTF(server);
+
+            player.sendPluginMessage(CommonUtils.getPlugin(), "BungeeCord", baos.toByteArray());
+        } catch (IOException e) {
+            CommonUtils.getPlugin().getLogger().severe(String.format("Error sending %s to server: %s",
+                    player.getName(),
+                    server));
+        }
+    }
+
+}
