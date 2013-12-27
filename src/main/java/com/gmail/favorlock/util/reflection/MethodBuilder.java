ea04d9d7b412147c77f1b416d96688e4ce4f0e53
Evan Lindsay <evanlindsay@live.com>
2013-12-27 09:45:31 -0800

    Simple MethodBuilder Should be good for clean code

---
+++ src/main/java/com/gmail/favorlock/util/reflection/MethodBuilder.java
@@ -0,0 +1,32 @@
+package com.gmail.favorlock.util.reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MethodBuilder {
+
+    Method method;
+    Object object;
+
+    public MethodBuilder(Class<?> clazz, String method, Object object) {
+        this.method = CommonReflection.getMethod(clazz, method);
+        this.object = object;
+    }
+
+    public MethodBuilder(Class<?> clazz, String method, Object object, Class<?>[] params) {
+        this.method = CommonReflection.getMethod(clazz, method, params);
+    }
+
+    public MethodBuilder invoke(Object... params) {
+        try {
+            method.invoke(object, params);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return this;
+    }
+
+}
