3350d1b8548eddd096e9cb455a65cce7799e5385
Evan Lindsay <evanlindsay@live.com>
2014-01-26 00:57:21 -0800

    Enable menu api in plugin main.

--- src/main/java/com/gmail/favorlock/CommonUtils.java
+++ src/main/java/com/gmail/favorlock/CommonUtils.java
@@ -1,17 +1,24 @@
 package com.gmail.favorlock;
 
+import com.gmail.favorlock.util.ui.MenuAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommonUtils extends JavaPlugin {
 
-    public static CommonUtils plugin;
+    private static CommonUtils plugin;
+    private static MenuAPI menuAPI;
 
     public void onEnable() {
         plugin = this;
+        menuAPI = new MenuAPI(this);
     }
 
     public static CommonUtils getPlugin() {
         return plugin;
+    }
+
+    public static MenuAPI getMenuAPI() {
+        return menuAPI;
     }
 
 }
